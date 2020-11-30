264d51c02c5bedfd333215d0dcd4cb16ad5478ce
Jorge Lucas <jorgelucas@pixelexperience.org>
2020-11-30 04:24:51 +0000

    lake: Remove build fingerprint from makefile

--- aosp_lake.mk
+++ aosp_lake.mk
@@ -75,5 +75,3 @@ PRODUCT_RELEASE_NAME := lake
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lake \
     PRIVATE_BUILD_DESC="lake-user 10 QPW30.61-21 cb81e release-keys"
-
-BUILD_FINGERPRINT := motorola/lake/lake:10/QPW30.61-21/cb81e:user/release-keys
