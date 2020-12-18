03ad25c619bb52e90c4d1c67186450ad57a38eea
Jorge Lucas <jorgelucas@pixelexperience.org>
2020-12-18 13:57:51 +0000

    [DNM]lake: Do not build FMRadio app

--- device.mk
+++ device.mk
@@ -56,11 +56,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
     $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 # Init
 PRODUCT_PACKAGES += \
     init.lake.rc
